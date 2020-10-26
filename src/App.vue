ec0a389b2dc813860142c23e5deef6c5da23066b
CJUSTNOW <2414964936@qq.com>
2020-10-26 14:49:05 +0800

    2222222

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="App">
     <h2>App-------APP</h2>
+    <Test />
   </div>
 </template>
 
 <script>
+import Test from './components/Test'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Test
+  }
 }
 </script>
 
---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test">
+    <h2>Test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+
+</style>
